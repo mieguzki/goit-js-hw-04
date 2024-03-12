422ef9977026df3e5d1697ca9fac4cec24a8b2dc
mieguzki <152374584+mieguzki@users.noreply.github.com>
2024-03-12 11:14:38 +0200

    tasks/1,2

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,43 @@
-function slugify(title) {
-    return title.toLowerCase().split(' ').join('-');
+
+// Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
+
+// Функція оголошує два параметри:
+
+// products — об’єкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів. Наприклад, { apples: 2, grapes: 4 }.
+// containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
+// Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. 
+// Тобто порахувати загальну кількість товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
+
+
+function isEnoughCapacity(products, containerSize) {
+  let sum = 0;
+  for (let product of Object.values(products)) {
+    sum += product;
   }
-  
-  
-  
-  console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-  console.log(slugify("English for developer")); // "english-for-developer"
-  console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-  console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+  return sum <= containerSize;
+}
+
+
+
+
+
+
+
+
+
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,27 @@
 
-function filterArray(numbers, value) {
-  const newArray = [];
-  for (let i = 0; i < numbers.length; i++) {
-      if (numbers[i] > value) {
-          newArray.push(numbers[i]);
-      }
-  }
-  return newArray;
-}
-  console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-  console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-  console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-  console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-  console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+// Об’єкт profile описує профіль користувача на ігровій платформі. 
+// У його властивостях зберігається ім’я профілю username та кількість активних годин playTime, проведених у грі.
 
+// const profile = {
+//     username: "Jacob",
+//   playTime: 300,
+// };
+
+// Доповни об’єкт profile методами для роботи з його властивостями.
+
+// Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
+// Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр hours та збільшити на нього значення властивості playTime. Нічого не повертає.
+// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!, де <Username> — це ім’я профілю, а <amount> — кількість ігрових годин.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
